af675e6b83e0e1f3418109935f7f6df0e917aa68
DanielSchned <danielschneider2a@gmail.com>
2021-02-28 15:47:14 +0100

    Retirement d'une ambition

--- src/Ambitions.js
+++ src/Ambitions.js
@@ -16,9 +16,9 @@ function Ambitions() {
         <div>       
           <p data-aos="fade-up" className="test" style = {{color: 'black', fontSize:70}}>Ambitions</p>
         <ul style = {{textAlign:'center'}}>
-          <li data-aos="fade-down" className="borderleft" style = {{color: 'black', fontSize:45, width: '30%', marginBottom:15}}>J'aimerais intégrer la licence à corte de developpeur fullstack</li>
+          <li data-aos="fade-down" className="borderleft" style = {{color: 'black', fontSize:45, width: '30%', marginBottom:15}}>J'aimerais intégrer la licence à corte de developpeur fullstack, puis continuer en master si possible</li>
           <li data-aos="fade-down" className="borderleft" style = {{color: 'black', fontSize:45, width: '30%', marginBottom:15}}>Je souhaite à la fin de mes études, de rentrer dans une équipe de developpeurs</li>
-          <li data-aos="fade-down" className="borderleft" style = {{color: 'black', fontSize:45, width: '30%', marginBottom:15}}>Je souhaite à la fin de mes études, de rentrer dans une équipe de developpeurs</li>
+
         </ul>
         
         </div>
