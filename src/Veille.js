e48d858e1bd03a32eeb9fcd61b1363f2d3462a40
DanielSchned <danielschneider2a@gmail.com>
2021-03-05 22:40:31 +0100

    Changement des textes de la veille

--- src/Veille.js
+++ src/Veille.js
@@ -27,18 +27,18 @@ function Veille() {
        
         <div className="test" style={{ marginTop:'5%' ,color: 'black', fontSize:55, textDecoration:'underline'}}> Twitter </div>
         <img className ='box' src={Twitter} style={{marginTop: 83, height: 400, width: 400, borderRadius:10, marginLeft:'-70%'}} />
-        <p className ="veille" style = {{color:'white', marginLeft:'30%', marginTop:'-27%', maxWidth:600, backgroundColor:'#2b2b2b', borderRadius: 5 }}>Je suis l'actualité sur Twitter, je suis toutes les informations qui m'intéresse tel que ReactNewsletter, Twitter est très performant car on peut recevoir un mail a chaque fois qu'il y a une nouveauté. Twitter lui sert à avoir l'actualité générale sur les langages de programmations WEB, c'est-à-dire les nouveautés en React, PHP etc..</p>
+        <p className ="veille" style = {{color:'white', marginLeft:'30%', marginTop:'-27%', maxWidth:600, backgroundColor:'#2b2b2b', borderRadius: 5 }}>Je suis toutes les informations qui m'intéresse sur Twitter tel que ReactNewsletter, ce réseau est très performant car il est possible de recevoir un mail à chaque nouveauté. Il sert égamelement à suivre l'actualité générale sur les langages de programmations WEB, c'est-à-dire les nouveautés en React, PHP etc..</p>
         <img data-aos="fade-left" className ='box' src={PreuveTwitter} style={{marginTop: "0%", borderRadius:10, marginLeft:"-6px"}} />
 
         <div className="test" style={{ marginTop:'15%' ,color: 'black', fontSize:55, textDecoration:'underline'}}> LeMonde </div>
         <img data-aos="fade-left" className ='box' src={LeMonde} style={{marginTop: 83, height: 400, width: 400, borderRadius:10, marginLeft:'-70%'}} />
-        <p data-aos="fade-left" className ="veille" style = {{color:'white', marginLeft:'30%', marginTop:'-27%', maxWidth:560, backgroundColor:'#2b2b2b', borderRadius: 5}}>Avec l'application LeMonde, je suis tenu d'actualité par ce qu'il se passe technologiquement, avec l'onglet 'pixel', de nombreuses informations sont essentielles, mais généralement, cela tourne autour de la cybersécurité, avec notamment la fuite de 500000 données médicales le 24 février 2021 </p>
+        <p data-aos="fade-left" className ="veille" style = {{color:'white', marginLeft:'30%', marginTop:'-27%', maxWidth:560, backgroundColor:'#2b2b2b', borderRadius: 5}}>Avec l'application LeMonde, il est possible de suivre l'actualité technologique avec l'onglet 'pixel'. Nombreuses informations sont essentielles, mais généralement, cela tourne autour de la cybersécurité, avec notamment la fuite de 500000 données médicales le 24 février 2021 </p>
         <img data-aos="fade-left" className ='box' src={PreuveLeMonde} style={{marginTop: "0%", borderRadius:10, marginLeft:"-44px"}} />
 
 
         <div data-aos = 'fade-left' className="test" style={{ marginTop:'15%' ,color: 'black', fontSize:55, textDecoration:'underline'}}> Youtube </div>
         <img data-aos="fade-left" className ='box' src={Youtube} style={{marginTop: 83, height: 400, width: 400, borderRadius:10, marginLeft:'-70%'}} />
-        <p data-aos="fade-left" className ="veille" style = {{color:'white', marginLeft:'30%', marginTop:'-27%', maxWidth:400, backgroundColor:'#2b2b2b', borderRadius: 5}}>Avec Youtube, je suis notifié des vidéos que les créateurs sortent, en conséquence, je suis tenu à l'actualité lorsqu'une nouveautée sort corcernant la programmation Web. Notamment avec la chaîne "Le designer du web" qui en plus de faire des tutoriels, il informe sur des nombreuses pratiques qui peuvent être réalisées en programmation web.</p>
+        <p data-aos="fade-left" className ="veille" style = {{color:'white', marginLeft:'30%', marginTop:'-27%', maxWidth:400, backgroundColor:'#2b2b2b', borderRadius: 5}}>Avec Youtube, lorsque de nouvelles vidéos sont publiées, il existe un système de notification qui permet de toujours rester d'actualité. En conséquence, celui-ci indique lorsqu'une vidéo concernant la programmation WEB est mise en ligne sur la platforme. Notamment avec la chaîne "Le designer du web" qui en plus de faire des tutoriels, il informe sur des nombreuses pratiques qui peuvent être réalisées en programmagion WEB.</p>
         <img data-aos="fade-left" className ='box' src={PreuveYoutube} style={{marginTop: "0%", borderRadius:10, marginLeft:"-24.5%"}} />
 
         <div style = {{marginTop: 210}}></div>
